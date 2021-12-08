d71f0c66acdfac2b9973fa79c806783da23c7d52
Adam Hinton <adam.d.hinton@gmail.com>
2021-12-07 20:10:17 -0500

    done with 'Find the HTML element that should trigger a deletion in the movie component'

--- src/reducers/movieReducer.js
+++ src/reducers/movieReducer.js
@@ -1,3 +1,4 @@
+import { tryStatement } from '@babel/types';
 import { ADD_MOVIE, DELETE_MOVIE } from '../actions/movieActions.js';
 import movies from './../data.js';
 
@@ -10,8 +11,21 @@ const reducer = (state = initialState, action) => {
     switch(action.type) {
         case DELETE_MOVIE:
             return {
+                ...state,
                 movies: state.movies.filter(item=>(action.payload !== item.id))
             }
+
+        case ADD_MOVIE:
+            const newMovie ={
+                ...action.payload,
+                id: Date.now()
+            }
+
+            return{
+                ...state,
+                movies: [...state.movies, newMovie]
+            }
+
         default:
             return state;
     }
